b4ec12e9674c1cd1377ee89c1ac35d428cadb125
sholanejo <78520217+sholanejo@users.noreply.github.com>
2022-01-02 11:12:18 +0100

    refactored code to use EJS templating engine

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const path = require('path');
 const app = new express();
+const ejs = require('ejs');
+app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/post', (req, res) => {
+    res.render('post')
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+})
 
 app.listen(4000, () => {
     console.log('App listening on port 4000')
