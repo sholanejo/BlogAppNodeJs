5fcf8a04a4f9f88f9d012f9df117747b277d75c9
Busari ThankGod <busarithienkie@gmail.com>
2022-01-05 12:07:54 +0100

    feat: handled error message for register

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -4,6 +4,8 @@ const path = require('path')
 module.exports = (req, res) => {
     User.create(req.body, (error, user) => {
         if (error) {
+            const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message);
+            req.session.validationErrors = validationErrors
             return res.redirect('/auth/register')
         }
         res.redirect('/')
