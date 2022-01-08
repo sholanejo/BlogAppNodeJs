e33ca355a912e0e6dba59b75c3702fbfe5c956b8
Shola Nejo <sholanejo@live.com>
2022-01-08 15:47:31 +0100

    added a what you see is what you get editor

--- controllers/newPost.js
+++ controllers/newPost.js
@@ -1,6 +1,8 @@
 module.exports = (req, res) => {
     if (req.session.userId) {
-        res.render('create')
+        res.render('create', {
+            createPost: true
+        })
     }
     res.redirect('/auth/login')
 
