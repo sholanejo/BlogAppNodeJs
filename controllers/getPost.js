62292d72c275c38852cf616a7600f0865416752d
Shola Nejo <sholanejo@live.com>
2022-01-08 14:55:24 +0100

    linked user and post collection to show postedBy current logged in user

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -1,7 +1,7 @@
 const BlogPost = require('../models/BlogPost.js')
 
 module.exports = async(req, res) => {
-    const blogpost = await BlogPost.findById(req.params.id)
+    const blogpost = await BlogPost.findById(req.params.id).populate('userid');
     res.render('post', {
         blogpost
     })
